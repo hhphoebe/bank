d1918544b594fb4fe47595a653c9685b7f88cdcb
e <1321516988@qq.com>
2019-04-11 10:59:41 +0800

    客户分类路径修改

---
+++ src/pages/user/classify/loClassify.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Layout} from 'antd';
+import LoraBread from '../../../commons/breadcrumb';
+
+const { Content } = Layout;
+
+export default class LoanClassify extends React.Component {
+    state = {
+        breads: [
+            {
+                href: 'classify',
+                text: '客户分类'
+            }
+        ],
+        title: '贷款用户',
+    }
+    render() {
+        const { breads, title } = this.state;
+        return (
+            <Content>
+                <div className="content">
+                    <LoraBread breads={breads} title={title}/>
+                </div>
+            </Content>
+        )
+    }
+}
